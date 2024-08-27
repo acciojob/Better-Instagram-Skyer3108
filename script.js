46e8466c7f85e5d9bd3adaf2fe7d49254f45efa1
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 17:23:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const parent = document.getElementById('parent');
+  let draggedElement = null;
+
+  parent.addEventListener('dragstart', (event) => {
+    draggedElement = event.target;
+    event.target.style.opacity = 0.5;
+  });
+
+  parent.addEventListener('dragend', (event) => {
+    event.target.style.opacity = ""; 
+  });
+
+  parent.addEventListener('dragover', (event) => {
+    event.preventDefault(); 
+  });
+
+  parent.addEventListener('drop', (event) => {
+    event.preventDefault();
+    if (event.target.className === 'image' && event.target !== draggedElement) {
+  
+      const parent = document.getElementById('parent');
+      parent.insertBefore(draggedElement, event.target.nextSibling);
+      parent.insertBefore(event.target, draggedElement);
+    }
+  });
+});
